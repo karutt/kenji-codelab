0812d97fcb0323df5fe3a185a989a3360e0442ea
karutt <karutetto332@gmail.com>
2025-05-04 06:18:59 +0900

    feat: 問題項目コンポーネントの初期値設定を改善
    ProblemItemコンポーネントで、parseProblemContentから取得したdefaultCodeを使用して初期値を設定するように変更しました。これにより、コードが指定されていない場合でも適切な初期値が表示されるようになります。

--- src/features/problem/components/ProblemItem.jsx
+++ src/features/problem/components/ProblemItem.jsx
@@ -3,13 +3,15 @@ import CodeEditorPanel from "./CodeEditorPanel";
 import SubmitButton from "./SubmitButton";
 
 export default function ProblemItem({ index, header, code, loading, onChange, onSubmit }) {
-    const { problemContent } = parseProblemContent(header);
+    const { problemContent, defaultCode } = parseProblemContent(header);
+
+    const initialValue = code || defaultCode;
 
     return (
         <section style={{ marginBottom: 32 }}>
             <h3>{header.textContent}</h3>
             <div dangerouslySetInnerHTML={{ __html: problemContent }} />
-            <CodeEditorPanel value={code} onChange={(v) => onChange(index, v)} />
+            <CodeEditorPanel value={initialValue} onChange={(v) => onChange(index, v)} />
             <SubmitButton loading={loading} onClick={() => onSubmit(index)} />
         </section>
     );
